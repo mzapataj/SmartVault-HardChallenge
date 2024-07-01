05304383a97bd08df324e96af0fdbd0f216681b6
jorge zapata <jorgerito1997@gmail.com>
2024-07-01 06:05:09 -0500

    Implemented functionality in Program.cs files

--- SmartVault.DataGeneration/Program.cs
+++ SmartVault.DataGeneration/Program.cs
@@ -1,13 +1,14 @@
-﻿using Dapper;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using SmartVault.Library;
+﻿using Microsoft.Extensions.Configuration;
+using SmartVault.Core.Settings;
+using SmartVault.DataGeneration.Interfaces;
+using SmartVault.DataGeneration.Services;
+using SmartVault.Shared.Interfaces;
+using SmartVault.Shared.Sql;
 using System;
-using System.Collections.Generic;
-using System.Data.SQLite;
+using System.Data;
 using System.IO;
-using System.Linq;
-using System.Xml.Serialization;
+using Microsoft.Data.Sqlite;
+using SmartVault.Shared.Data;
 
 namespace SmartVault.DataGeneration
 {
@@ -19,106 +20,19 @@ namespace SmartVault.DataGeneration
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json").Build();
 
-            SQLiteConnection.CreateFile(configuration["DatabaseFileName"]);
-            File.WriteAllText("TestDoc.txt", $"This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}This is my test document{Environment.NewLine}");
-            var documentPath = new FileInfo("TestDoc.txt").FullName;
+            var settings = configuration.Get<Settings>();
 
-            var files = Directory.GetFiles(@"..\..\..\..\BusinessObjectSchema");            
-
-            using (var connection = new SQLiteConnection(string.Format(configuration?["ConnectionStrings:DefaultConnection"] ?? "", configuration?["DatabaseFileName"])))
-            {
-                CreateTables(files, connection);
-                
-                connection.Open();
-
-                using (var transaction = connection.BeginTransaction())
-                {
-                    var documentNumber = 0;
-
-                    var userInsert = @"INSERT INTO User (Id, FirstName, LastName, DateOfBirth, AccountId, Username, Password) 
-                    VALUES ($index, 'FName' || $index, 'LName' || $index,$randomDate,$index,
-                    'UserName-' || $index,'e10adc3949ba59abbe56e057f20f883e');
-
-                    INSERT INTO Account (Id, Name) VALUES($index,'Account' || $index);";
-
-                    var command = connection.CreateCommand();                    
-
-                    var indexParam = command.CreateParameter();
-                    indexParam.ParameterName = "$index";
-                    command.Parameters.Add(indexParam);
-                    
-                    var randomDateParam = command.CreateParameter();
-                    randomDateParam.ParameterName = "$randomDate";
-                    command.Parameters.Add(randomDateParam);
-
-                    var documentNumberParam = command.CreateParameter();
-                    documentNumberParam.ParameterName = "documentNumber";
-                    var documentNameParam = command.CreateParameter();
-                    documentNameParam.ParameterName = "$documentName";
-                    var documentPathParam = command.CreateParameter();
-                    documentPathParam.ParameterName = "$documentPath";
-                    var documentLengthParam = command.CreateParameter();
-                    documentLengthParam.ParameterName = "$documentLength";
-
-                    command.Parameters.Add(documentNumberParam);
-                    command.Parameters.Add(documentNameParam);
-                    command.Parameters.Add(documentPathParam);
-                    command.Parameters.Add(documentLengthParam);
-
-                    for (int i = 0; i < 100; i++)
-                    {
-                        var randomDayIterator = RandomDay().GetEnumerator();
-                        randomDayIterator.MoveNext();
-                        command.CommandText = userInsert;
-
-                        indexParam.Value = i;
-                        randomDateParam.Value = randomDayIterator.Current.ToString("yyyy-MM-dd");
-                        
-                        command.ExecuteNonQuery();
-
-                        command.CommandText = $"INSERT INTO Document (Id, Name, FilePath, Length, AccountId) VALUES ($documentNumber,$documentName,$documentPath,$documentLength,$index);";
-
-                        for (int d = 0; d < 10000; d++, documentNumber++)
-                        {
-                            documentNumberParam.Value = documentNumber;
-                            documentNameParam.Value = $"Document{i}-{d}.txt";
-                            documentPathParam.Value = documentPath;
-                            documentLengthParam.Value = new FileInfo(documentPath).Length;
-
-                            command.ExecuteNonQuery();
-                        }
-                    }
-                    transaction.Commit();
-                }
-
-                var accountData = connection.Query("SELECT COUNT(*) FROM Account;");
-                Console.WriteLine($"AccountCount: {JsonConvert.SerializeObject(accountData)}");
-                var documentData = connection.Query("SELECT COUNT(*) FROM Document;");
-                Console.WriteLine($"DocumentCount: {JsonConvert.SerializeObject(documentData)}");
-                var userData = connection.Query("SELECT COUNT(*) FROM User;");
-                Console.WriteLine($"UserCount: {JsonConvert.SerializeObject(userData)}");
-
-                connection.Close();
-            }
-        }
-
-        private static void CreateTables(string[] files, SQLiteConnection connection)
-        {
-            foreach (var file in files)
-            {
-                var serializer = new XmlSerializer(typeof(BusinessObject));
-                var businessObject = serializer.Deserialize(new StreamReader(file)) as BusinessObject;
-                connection.Execute(businessObject?.Script);
-            }
-        }
-
-        static IEnumerable<DateTime> RandomDay()
-        {
-            DateTime start = new DateTime(1985, 1, 1);
-            Random gen = new Random();
-            int range = (DateTime.Today - start).Days;
-            while (true)
-                yield return start.AddDays(gen.Next(range));
+            IDbConnection connection = new SqliteConnection(settings.DefaultConnection);
+            IDbCommandService command = new DbCommandService(); 
+            
+            IDbContextService dbContextService = new SQLiteDbContextService(connection, command);
+            IDataGenerationService dataGenerationService = new DataGenerationService(dbContextService, settings);
+            
+            var result = dataGenerationService.SeedDatabase();
+            
+            Console.WriteLine($"AccountCount: {result.accountData}");            
+            Console.WriteLine($"DocumentCount: {result.documentData}");            
+            Console.WriteLine($"UserCount: {result.userData}");                        
         }
     }
 }
--- SmartVault.Program/Program.cs
+++ SmartVault.Program/Program.cs
@@ -1,30 +1,55 @@
-﻿using SmartVault.Core.BusinessObjects;
+﻿using System;
+using System.Data;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using SmartVault.Core.Settings;
+using SmartVault.Infrastructure.Services;
+using SmartVault.Shared.Data;
+using SmartVault.Shared.Interfaces;
+using SmartVault.Shared.Sql;
 
 namespace SmartVault.Program
 {
     partial class Program
     {
+        private static IDocumentService documentService;
+        private static IDbContextService dbContextService;
+        private static IDbConnection connection;
+        private static IDbCommandService command;
         static void Main(string[] args)
         {
-            var xd = new Account()
+            if (args.Length == 0)
             {
-                Name = "popo"
+                return;
             }
-            ;
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            var settings = configuration.Get<Settings>();
+
+            connection = new SqliteConnection(settings.DefaultConnection);
+            command = new DbCommandService(); 
+            dbContextService = new SQLiteDbContextService(connection, command);
+            documentService = new DocumentService(dbContextService);
+            
             WriteEveryThirdFileToFile(args[0]);
             GetAllFileSizes();
         }
 
         private static void GetAllFileSizes()
         {
-            var lol = new Account();
-            
-            // TODO: Implement functionality
+            Console.WriteLine("WriteEveryThirdFileToFile method started");
+            var count = documentService.GetAllFileSizes();
+            Console.WriteLine($"{count} files were updated");
         }
 
         private static void WriteEveryThirdFileToFile(string accountId)
         {
-            // TODO: Implement functionality
+            Console.WriteLine("WriteEveryThirdFileToFile method started");
+            var count = documentService.WriteEveryThirdFileToFile(accountId);
+            Console.WriteLine($"{count} files contains '{documentService.TextToMatch}' text");
         }
     }
 }
